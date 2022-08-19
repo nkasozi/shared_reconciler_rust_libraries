bb0a154bc992fb62ece55f396b72e6f581820967
Nkasozi <Kasozi@grid.gg>
2022-08-19 03:57:45 +0300

    fix: make last_acknowledged_id an optional in file_chunk_queue entity

--- models/entities/file_chunk_queue.rs
+++ models/entities/file_chunk_queue.rs
@@ -3,5 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
 pub struct FileChunkQueue {
     pub queue_id: String,
-    pub last_acknowledged_id: String,
+    pub last_acknowledged_id: Option<String>,
 }
