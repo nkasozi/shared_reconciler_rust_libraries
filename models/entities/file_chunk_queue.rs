9f8905997f59ef7e6043636b625e73c633af0a18
Nkasozi <Kasozi@grid.gg>
2022-08-20 10:44:07 +0300

    refactor: rename queue_id in file_chunk_queue to topic_id inorder to simplify comprehension

--- models/entities/file_chunk_queue.rs
+++ models/entities/file_chunk_queue.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
+#[derive(Default, Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
 pub struct FileChunkQueue {
-    pub queue_id: String,
+    pub topic_id: String,
     pub last_acknowledged_id: Option<String>,
 }
