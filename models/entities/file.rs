8521ab2d81059ae851aa2d187d51947b82bae694
Nkasozi <Kasozi@grid.gg>
2022-08-26 11:21:10 +0300

    feature: Add datastructures for files and reconstructed_files

--- models/entities/file.rs
+++ models/entities/file.rs
@@ -5,6 +5,33 @@ use serde::{Deserialize, Serialize};
 pub struct File {
     pub id: String,
     pub upload_request_id: String,
+    pub file_storage_location: FileStorageLocation,
+    pub file_extension: SupportedFileExtension,
     pub file_path: Option<String>,
     pub file_type: ReconFileType,
+}
+
+#[derive(Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
+pub enum SupportedFileExtension {
+    Csv,
+    Excel,
+    Pdf,
+}
+
+impl Default for SupportedFileExtension {
+    fn default() -> Self {
+        SupportedFileExtension::Csv
+    }
+}
+
+#[derive(Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
+pub enum FileStorageLocation {
+    LocalFileSystem,
+    S3FileSystem,
+}
+
+impl Default for FileStorageLocation {
+    fn default() -> Self {
+        FileStorageLocation::LocalFileSystem
+    }
 }
