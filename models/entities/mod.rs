8529f54ce8f5d74e6fc5cb8a18ac9aa3b346ce46
Nkasozi <Kasozi@grid.gg>
2022-08-26 10:30:32 +0300

    feature: Add datastructures for files and reconstructed_files

--- models/entities/mod.rs
+++ models/entities/mod.rs
@@ -1,4 +1,6 @@
 pub mod app_errors;
+pub mod file;
 pub mod file_chunk_queue;
 pub mod file_upload_chunk;
 pub mod recon_tasks_models;
+pub mod reconstructed_file;
--- models/entities/reconstructed_file.rs
+++ models/entities/reconstructed_file.rs
@@ -7,5 +7,6 @@ use super::file_upload_chunk::FileUploadChunk;
 #[derive(Default, Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
 pub struct ReconstructedFile {
     pub file_id: String,
+    pub upload_request_id: String,
     pub file_chunk_id_to_chunk_details_map: HashMap<i64, FileUploadChunk>,
 }
