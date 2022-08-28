6b7c694dc61e05325f8159fe2788f5e90848b0ac
Nkasozi <Kasozi@grid.gg>
2022-08-29 00:43:32 +0300

    feature: Add the FileHasBeenRead entity such that we can represent that intermediate state

--- models/entities/file.rs
+++ models/entities/file.rs
@@ -1,6 +1,8 @@
 use crate::internal::shared_reconciler_rust_libraries::models::entities::recon_tasks_models::ReconFileType;
 use serde::{Deserialize, Serialize};
 
+use super::file_row::FileRow;
+
 #[derive(Default, Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
 pub struct File {
     pub id: String,
@@ -9,6 +11,14 @@ pub struct File {
     pub file_extension: SupportedFileExtension,
     pub file_path: Option<String>,
     pub file_type: ReconFileType,
+}
+
+pub struct FileThatHasBeenRead {
+    pub id: String,
+    pub upload_request_id: String,
+    pub file_type: ReconFileType,
+    pub column_headers: Vec<String>,
+    pub file_rows: Vec<FileRow>,
 }
 
 #[derive(Serialize, PartialEq, Clone, Eq, Deserialize, Debug)]
