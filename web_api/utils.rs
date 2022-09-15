fa94833ca80b3dfa459e1f4b444ffb31e2d7d179
Nkasozi <Kasozi@grid.gg>
2022-09-16 01:42:06 +0300

    feature: introduce a shared web_api module that holds shared code for web_api files

--- web_api/utils.rs
+++ web_api/utils.rs
@@ -1,14 +1,27 @@
 use actix_web::HttpResponse;
 use serde::Serialize;
+
 use crate::internal::shared_reconciler_rust_libraries::models::entities::app_errors::{AppError, AppErrorKind};
 
-pub fn ok_or_error<T: Serialize> (response:Result<T,AppError>) -> HttpResponse{
+pub fn ok_or_error<T: Serialize>(response: Result<T, AppError>) -> HttpResponse {
     return match response {
         Ok(details) => HttpResponse::Ok().json(details),
         Err(err) => match err.kind {
-            AppErrorKind::NotFound => HttpResponse::NotFound().json(format!("{}", err)),
-            AppErrorKind::BadClientRequest => HttpResponse::BadRequest().json(format!("{}", err)),
-            _ => HttpResponse::InternalServerError().json(format!("{}", err)),
+            AppErrorKind::NotFound => not_found_error(err),
+            AppErrorKind::BadClientRequest => bad_client_request(err),
+            _ => internal_server_error(err),
         },
     };
+}
+
+pub fn internal_server_error(error: AppError) -> HttpResponse {
+    return HttpResponse::InternalServerError().json(format!("{}", error));
+}
+
+pub fn bad_client_request(error: AppError) -> HttpResponse {
+    return HttpResponse::BadRequest().json(format!("{}", error));
+}
+
+pub fn not_found_error(error: AppError) -> HttpResponse {
+    return HttpResponse::NotFound().json(format!("{}", error));
 }
